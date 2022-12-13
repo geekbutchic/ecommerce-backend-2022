02595648f57ed3573b25418458cd51058c925878
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2022-12-13 09:36:05 -0700

    adds /api to routes

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/server", serverRouter);
+app.use("/api", indexRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/server", serverRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
